e006f1c7e1042acdd3bcecd4097e884a45a4cb97
Simplify-ERP® <github@euroblaze.de>
2023-03-27 22:13:19 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,13 @@
-
+{
+    'name': 'Customer Invoices Report',
+    'version': '1.0',
+    'category': 'Accounting/Accounting',
+    'summary': 'Generate a PDF report of customer invoices',
+    'author': 'Ashant Chalasani, assisted by AI',
+    'depends': ['account'],
+    'data': [
+        'views/customer_invoices_report.xml',
+        'reports/customer_invoices_report_template.xml',
+    ],
+    'application': False,
+}
